d4026210baedf22f9686ab7255cd1a23cc7f1dc9
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2022-01-01 21:17:59 +0600

    user backend

--- Router/usersRouter.js
+++ Router/usersRouter.js
@@ -3,10 +3,24 @@ const express = require("express");
 
 // internal imports
 const router = express.Router();
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+   addUserValidators,
+   addUserValidatorHandler,
+} = require("../middlewares/users/userValidator");
 
 // users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post(
+   "/",
+   avatarUpload,
+   addUserValidators,
+   addUserValidatorHandler,
+   addUser
+);
 
 module.exports = router;
---
+++ utilities/singleUploader.js
@@ -0,0 +1,44 @@
+const createError = require("http-errors");
+
+function uploader(subfolder_path, allowed_file_size, max_file_size, error_msg) {
+   // make upload object
+   const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}/`;
+
+   // define the storage
+   const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+         cb(null, UPLOADS_FOLDER);
+      },
+      filename: (req, file, cb) => {
+         const fileExt = path.extname(file.originalname);
+         const fileName =
+            file.originalname
+               .replace(fileExt, "")
+               .toLowerCase()
+               .split(" ")
+               .join("-") +
+            "-" +
+            Date.now();
+
+         cb(null, fileName + fileExt);
+      },
+   });
+
+   // prepare the final multer upload object
+   const upload = multer({
+      storage: storage,
+      limits: {
+         fileSize: max_file_size,
+      },
+      fileFilter: (req, file, cb) => {
+         if (allowed_file_size.includes(file.mimetype)) {
+            cb(null, true);
+         } else {
+            cb(createError(error_msg));
+         }
+      },
+   });
+
+   return upload;
+}
+module.exports = uploader;
